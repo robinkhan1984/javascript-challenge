24cecc3976b36b8964517a9b29f8d1db7ba71300
unknown <robinkhan1984@gmail.com>
2020-12-10 20:18:12 -0500

    creating app.js

---
+++ static/js/app.js
@@ -0,0 +1,29 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var button = d3.select("#button")
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
